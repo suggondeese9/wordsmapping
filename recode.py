2931ed31d58fc08b05d77125f354a9ea32a57aa3
sugondeese9 <sugon.deese9@gmail.com>
2024-09-10 00:11:59 +0000

    a

--- recode.py
+++ recode.py
@@ -5,11 +5,14 @@ import os
 # Input string
 # Check if the user provided an input string
 if len(sys.argv) < 2:
-    print("Usage: python script.py <wordsfile>")
+    print("Usage: python script.py <wordsfile> [-i]")
     sys.exit(1)
 
 # Input string from command line arguments
 filename = sys.argv[1]
+inverse=False
+if len(sys.argv)==3 and sys.argv[2]=='-i':
+   inverse=True
 
 # Read the file 'words.txt' and sort the words alphabetically, then print the list
 
@@ -25,33 +28,34 @@ for word in words:
     word=word.split(' ')
     if len(word)==1:
         word.append('')
-    word[1]=word[1].replace('BB','11')
-    word[1]=word[1].replace('BJ','22')
-    word[1]=word[1].replace('DT','84')
-    word[1]=word[1].replace('FS','42')
-    word[1]=word[1].replace('CO','32')
-    word[1]=word[1].replace('CI','33')
-    word[1]=word[1].replace('AT','41')
-    word[1]=word[1].replace('FI','31')
-    word[1]=word[1].replace('FK','34')
-    word[1]=word[1].replace('SO','20')
-    word[1]=word[1].replace('QUIR','9999')
-    word[1]=word[1].replace('SE','23')
-    word[1]=word[1].replace('FE','21')
-    print(f'{word[0]} {word[1]}')    
 
-if False:
-    word[1]=word[1].replace('11','BB')
-    word[1]=word[1].replace('22','BJ')
-    word[1]=word[1].replace('84', 'DT')
-    word[1]=word[1].replace('42', 'FS')
-    word[1]=word[1].replace('32', 'CO')
-    word[1]=word[1].replace('33', 'CI')
-    word[1]=word[1].replace('41', 'AT')
-    word[1]=word[1].replace('31', 'FI')
-    word[1]=word[1].replace('34', 'FK')
-    word[1]=word[1].replace('20', 'SO')
-    word[1]=word[1].replace('9999', 'QUIR')
-    word[1]=word[1].replace('23', 'SE')
-    word[1]=word[1].replace('21', 'FE')
+    if inverse == False:
+      word[1]=word[1].replace('BB','11')
+      word[1]=word[1].replace('BJ','22')
+      word[1]=word[1].replace('DT','84')
+      word[1]=word[1].replace('FS','42')
+      word[1]=word[1].replace('CO','32')
+      word[1]=word[1].replace('CI','33')
+      word[1]=word[1].replace('AT','41')
+      word[1]=word[1].replace('FI','31')
+      word[1]=word[1].replace('FK','34')
+      word[1]=word[1].replace('SO','20')
+      word[1]=word[1].replace('QUIR','9999')
+      word[1]=word[1].replace('SE','23')
+      word[1]=word[1].replace('FE','21')
+    else:
+      word[1]=word[1].replace('11','BB')
+      word[1]=word[1].replace('22','BJ')
+      word[1]=word[1].replace('84', 'DT')
+      word[1]=word[1].replace('42', 'FS')
+      word[1]=word[1].replace('32', 'CO')
+      word[1]=word[1].replace('33', 'CI')
+      word[1]=word[1].replace('41', 'AT')
+      word[1]=word[1].replace('31', 'FI')
+      word[1]=word[1].replace('34', 'FK')
+      word[1]=word[1].replace('20', 'SO')
+      word[1]=word[1].replace('9999', 'QUIR')
+      word[1]=word[1].replace('23', 'SE')
+      word[1]=word[1].replace('21', 'FE')
+
     print(f'{word[0]} {word[1]}')    
